2d0f69575ed77aa4f6b1aea92e44aae812800aba
Csilla <posa.csilla@gmail.com>
2021-06-01 11:51:47 +0200

    Setting up picture-in-picture in js
     To capture live stream contents (user's choice) and playing them in the video element.

---
+++ script.js
@@ -0,0 +1,22 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+//prompt to select media stream, pass to video element, then play
+//https://css-tricks.com/an-introduction-to-the-picture-in-picture-web-api/
+async function selectMediaStream() {
+    try {
+        //to capture live stream contents
+        //user can select what to play (getDisplayMedia())
+        //it plays in the videoElement
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        //catch error here
+    }
+}
+
+//on load
+selectMediaStream();
