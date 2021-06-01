59f2b91a070cb0572005338e53a235189a18ef60
Csilla <posa.csilla@gmail.com>
2021-06-01 11:59:35 +0200

    Code for button functionality
    To be able to start, disable and reset button.

--- script.js
+++ script.js
@@ -18,5 +18,14 @@ async function selectMediaStream() {
     }
 }
 
+button.addEventListener('click', async () => {
+    //disable button
+    button.disabled = true;
+    //start picture in picture
+    await videoElement.requestPictureInPicture();
+    //reset button
+    button.disabled = false;
+});
+
 //on load
 selectMediaStream();
